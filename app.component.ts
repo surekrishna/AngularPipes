c1c2e5d6765e34e0d9c6b04ef38b61e6f757d257
Krishna <surekrrishna@gmail.com>
2018-03-09 00:26:22 +0530

    src/app folder files
    Added src/app folder files from AngularPipes

---
+++ app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  items = ["Angular","Bootstrap","Java"];
+  newItem = "";
+  pushItem = function(){
+    if(this.newItem != ""){
+      this.items.push(this.newItem);
+      this.newItem = "";
+    }
+  }
+  removeItem = function(index){
+    this.items.splice(index,1);
+  }
+}
